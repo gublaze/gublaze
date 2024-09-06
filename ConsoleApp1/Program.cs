2b86068e889fc313baae9c517e26803e746128ca
razakov <uldybaevamilena@gmail.com>
2024-09-06 09:50:15 +0500

    ber commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
+        public static void guliana()
+        {
+            double prom = 0; double summa = 0;
+            Console.WriteLine("Введите число до 10"); int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                prom = Math.Pow(x, i) / (x + i); summa += prom;
+            }
+            Console.WriteLine(summa);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           guliana();
+            Console.ReadKey();
         }
     }
 }
