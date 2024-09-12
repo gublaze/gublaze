f34975ec30f92aa21b4fb240f9041322d18e042b
gublaze <uldybaevamilena@gmail.com>
2024-09-12 09:12:24 +0500

    berayy

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace guliana
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = -10; i <= 10; i++)
+            {
+                Console.WriteLine($"число:{i}, результат:{ProcessNumber(i)}");
+            }
+        }
+        static double ProcessNumber(int number)
+        {
+            if (number > 0)
+            {
+                return Math.Pow(number, 2);
+            }
+            else if (number < 0)
+            {
+                return Math.Pow(number, 3);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
